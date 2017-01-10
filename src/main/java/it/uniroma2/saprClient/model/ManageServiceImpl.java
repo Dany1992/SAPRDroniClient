2e33b581ab17a9bfad46ae3c7c507c08bc943835
“TommasinoPierfrancesco” <tommasinofrancesco@hotmail.it>
2017-01-10 22:26:40 +0100

    commi di qualcuno

--- src/main/java/it/uniroma2/saprClient/model/ManageServiceImpl.java
+++ src/main/java/it/uniroma2/saprClient/model/ManageServiceImpl.java
@@ -4,9 +4,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Logger;
 
+import javax.servlet.http.HttpServletRequest;
+
 import it.uniroma2.sapr.service.Exception_Exception;
 import it.uniroma2.sapr.service.Operation;
 import it.uniroma2.sapr.service.Opzione;
+import it.uniroma2.sapr.service.RequestCheckElement;
+import it.uniroma2.sapr.service.RequestDevice;
 import it.uniroma2.sapr.service.RequestFlightPlan;
 import it.uniroma2.sapr.service.RequestPilot;
 import it.uniroma2.sapr.service.ResponseDevice;
@@ -16,9 +20,11 @@ import it.uniroma2.sapr.service.SAPRDroniInterface;
 import it.uniroma2.saprClient.view.FlightPlan;
 import it.uniroma2.saprClient.view.FlightPlanWrapper;
 import it.uniroma2.saprClient.view.Pilot;
-import java.net.MalformedURLException;
-import java.net.URL;
+import it.uniroma2.saprClient.view.Device;
+import java.util.Iterator;
+import java.util.List;
 import java.util.ArrayList;
+
 
 public class ManageServiceImpl implements ManageService {
 	String clazz = "MangeServiceImpl";
@@ -79,6 +85,62 @@ public class ManageServiceImpl implements ManageService {
 		return result;
 	}
 	
+	public Boolean addDevice(Device d) {
+		String method = "addDevice";
+		Boolean result = false;
+		logger.info(String.format("Class:%s-Method:%s::START with dates[%s]", clazz,method,d.toString()));
+		RequestDevice rq = new RequestDevice();
+		rq.setOperation(Operation.ADD);
+		buildMapDevice(d, rq);
+		
+		try {
+			result = service.managerDevice(rq);
+			logger.info(String.format("Class:%s-Method:%s:: Call to WebService Success", clazz,method,result));
+		} catch (Exception_Exception e) {
+			System.out.println(e.toString());
+			logger.info(String.format("Class:%s-Method:%s:: Call to WebService error[%s]", clazz,method,e.toString()));
+			return false;
+		}
+		
+		logger.info(String.format("Class:%s-Method:%s::END with result[%b]", clazz,method,result));
+		return result;
+	}
+        
+	public List<ResponseDevice> selectDevices(String pilotLicense) {
+		String method = "selectDevicesOfPilot";
+		logger.info(String.format("Class:%s-Method:%s::START with dates[%s]", clazz,method,pilotLicense));
+                List<ResponseDevice> devices = null;
+                try{
+                    devices = service.getDevicesOfPilot(Opzione.ENABLED, pilotLicense);
+                } catch (Exception_Exception e) {
+			e.printStackTrace();
+		}
+		logger.info(String.format("Class:%s-Method:%s::END with result[%b]", clazz,method,devices));
+		return devices;
+	}
+
+	public Boolean removeDevice(Device d) {
+		String method = "removeDevice";
+		Boolean result = false;
+		logger.info(String.format("Class:%s-Method:%s::START with dates[%s]", clazz,method,d.toString()));
+		RequestDevice rq = new RequestDevice();
+		rq.setOperation(Operation.DELETE);
+		buildMapDevice(d, rq);
+		
+		try {
+			result = service.managerDevice(rq);
+			logger.info(String.format("Class:%s-Method:%s:: Call to WebService Success", clazz,method,result));
+		} catch (Exception_Exception e) {
+			System.out.println(e.toString());
+			logger.info(String.format("Class:%s-Method:%s:: Call to WebService error[%s]", clazz,method,e.toString()));
+			result = false;
+		}
+		
+		logger.info(String.format("Class:%s-Method:%s::END with result[%b]", clazz,method,result));
+		return result;
+	}
+        
+        
 	private void buildMapPilot(Pilot p, RequestPilot rq) {
 		String method = "buildMapPilot";
 		logger.info(String.format("Class:%s-Method:%s::START with dates", clazz,method));
@@ -117,6 +179,28 @@ public class ManageServiceImpl implements ManageService {
 		logger.info(String.format("Class:%s-Method:%s::END with result[%b]", clazz,method,result));
 		return result;
 	}
+
+
+
+
+        private void buildMapDevice(Device d, RequestDevice rq) {
+                String method = "buildMapDevice";
+		logger.info(String.format("Class:%s-Method:%s::START with dates", clazz,method));
+		rq.setIdDevice(d.getIdDevice());
+                rq.setModel(d.getModel());
+                rq.setTyper(d.getType());
+                rq.setWeight(d.getWeight());
+                rq.setProducer(d.getProducer());
+                rq.setPilotLicense(d.getPilotLicense());
+                
+                RequestCheckElement checkElement = new RequestCheckElement();
+                for (Iterator iterator = d.getCheckDevice().iterator(); iterator.hasNext();) {
+                    checkElement.setValues(String.valueOf(iterator));
+                    rq.getChekDevice().set(0, checkElement);
+                }
+                
+		logger.info(String.format("Class:%s-Method:%s::END", clazz,method));
+        }
 
 
 	public Boolean removeFlightPlan(FlightPlan p) {
@@ -202,16 +286,51 @@ public class ManageServiceImpl implements ManageService {
 		ArrayList<ResponseDevice> devicesOfPilot = null;
 		FlightPlan willFlight = new FlightPlan();
 		try {
+			System.out.println("Richiamo il Web-Method getSaprsOfPilot");
 			saprsOfPilot = (ArrayList<ResponseSapr>) service.getSaprsOfPilot(Opzione.ENABLED, pilotLicense);
+			System.out.println(saprsOfPilot.toString());
+			System.out.println("Richiamo il Web-Method getDevicesOfPilot");
 			devicesOfPilot = (ArrayList<ResponseDevice>) service.getDevicesOfPilot(Opzione.ENABLED, pilotLicense);
+			System.out.println(devicesOfPilot.toString());
 		} catch (Exception_Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Errore prelevamento dati Web-Service: "+ e.toString());
 		}
 		
 		flight.setDevicesOfPilot(devicesOfPilot);
 		flight.setSaprsOfPilot(saprsOfPilot);
 		flight.setFlight(willFlight);
 		return flight;
+	}
+
+    public Boolean addDevice(it.uniroma2.sapr.service.Device device) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Boolean removeDevice(it.uniroma2.sapr.service.Device device) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+	public void setFlightPlan(HttpServletRequest request, FlightPlan flightPlan) {
+		flightPlan.setDestinations(request.getParameter("destinations"));
+		
+		flightPlan.setDeparture(request.getParameter("departure"));
+		
+		flightPlan.setDateDeparture(request.getParameter("dateDeparture"));
+		
+		flightPlan.setTimeDeparture(request.getParameter("timeDeparture"));
+		
+		flightPlan.setNowArriving(request.getParameter("nowArriving"));
+		
+		int idNote = Integer.parseInt(request.getParameter("idNote"));
+		flightPlan.setIdNote(idNote);
+		
+		flightPlan.setPilotLicense(request.getParameter("pilotLicense"));
+		
+		int idSapr = Integer.parseInt(request.getParameter("idSapr"));
+		flightPlan.setIdSapr(idSapr);
+		
+		ArrayList<Integer> devices = new ArrayList<Integer>();
+		devices.
+		flightPlan.setDevices(devices);
 	}
 }
